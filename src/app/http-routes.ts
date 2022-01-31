adafa7f2be192be5ab0a5332b02ce0858880553c
Pablo Bento <56163601+bento-code@users.noreply.github.com>
2022-01-31 17:28:34 +0100

    production mode configured

--- src/app/http-routes.ts
+++ src/app/http-routes.ts
@@ -1,7 +1,7 @@
 export abstract class HttpRoutes 
 {
     //false => localhost routes. true => production routes;
-    static readonly production=false;
+    static readonly production=true;
     
     static readonly REST_API_URL=this.production ? 'https://chess-rook-rest-api.herokuapp.com' : 'http://localhost:3000';
     static readonly USER_URL= this.REST_API_URL + '/user';
